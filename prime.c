285ee0650632fb4ef3d66f678cc0eb02f952101e
ducluong <114284188+hungductdm@users.noreply.github.com>
2023-08-12 10:35:41 +0700

    Update prime.c
    Added more comments

--- prime.c
+++ prime.c
@@ -7,6 +7,7 @@ bool prime(int number);
 int main(void)
 {
     int min;
+    // Ensure user enters a Minimum value larger than 0.
     do
     {
         min = get_int("Minimum: ");
@@ -14,12 +15,15 @@ int main(void)
     while (min < 1);
 
     int max;
+    // Ensure user enters a Maximum value larger than Minimum value entered previously.
     do
     {
         max = get_int("Maximum: ");
     }
     while (min >= max);
 
+    // Loop through each interger number between min and max and apply prime function.
+    // If prime() returns true, print that number to screen.
     for (int i = min; i <= max; i++)
     {
         if (prime(i))
@@ -32,13 +36,18 @@ int main(void)
 bool prime(int number)
 {
     // TODO
+    
+    /* 
+    1 is not a prime number and is handled separately as it does not meet the condition i = 2
+    of the trial division (the "for" loop) below. Therefore, 1 will return true, which is incorrect.
+    */ 
     if (number == 1)
     {
         return false;
     }
 
     // Trial division between 2 up to square root number (truncated) to check its primality
-    for (int i = 2; i <= (number / 2); i++)
+    for (int i = 2; i <= sqrt(number); i++)
     {
         if (number % i == 0)
         {
